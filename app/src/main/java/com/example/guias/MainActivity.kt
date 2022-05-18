ba19d3a3fbc55c6762dcfd72e7fbc08902009290
melissafg <melissa.flores.g@ucb.edu.bo>
2022-05-17 21:19:06 -0400

    first commit

---
+++ app/src/main/java/com/example/guias/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.example.guias
+
+import android.graphics.Color
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import androidx.constraintlayout.widget.ConstraintLayout
+
+
+class MainActivity : AppCompatActivity() {
+
+    private val redBtn : Button
+        get() = findViewById(R.id.red_btn)
+
+    private val whiteBtn : Button
+        get() = findViewById(R.id.white_btn)
+
+    private val layoutPrincipal : ConstraintLayout
+        get() = findViewById(R.id.layout_main)
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        redBtn.setOnClickListener{
+            toast("Rojo")
+            layoutPrincipal.setBackgroundColor(Color.RED)
+        }
+
+        whiteBtn.setOnClickListener{
+            toast("Blanco")
+            layoutPrincipal.setBackgroundColor(Color.WHITE)
+        }
+    }
+    private fun toast(text: String, duration: Int = Toast.LENGTH_LONG)=
+        Toast.makeText(this@MainActivity, text, Toast.LENGTH_LONG).show()
+}
