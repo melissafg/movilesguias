4eb14d1a3fb2f06038af0e6d54f165b8bc8b5130
melissafg <melissa.flores.g@ucb.edu.bo>
2022-06-01 11:09:03 -0400

    pasos de la guia

--- app/src/main/java/com/example/guias/MainActivity.kt
+++ app/src/main/java/com/example/guias/MainActivity.kt
@@ -1,44 +1,16 @@
 package com.example.guias
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import android.view.Menu
-import android.view.MenuItem
-import android.view.View
-import android.widget.Toast
+import com.google.android.material.snackbar.Snackbar
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_main);
+        Snackbar.make(layoutPrincipal, getString(R.string.mensaje_snackbar), Snackbar.LENGTH_LONG).show()
     }
-    fun selectedButton(view: View) {
-        if ( view.id === R.id.btnServices) {
-            val intent: Intent = Intent(view.context, MainActivity::class.java)
-            startActivity(intent)
-
-        } else {
-            Toast.makeText( view.context, "Evento no definido", Toast.LENGTH_LONG).show()
-        }
-    }
-
-
-    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        menuInflater.inflate(R.menu.menu_main, menu)
-        return true
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        when (item!!.itemId) {
-
-            R.id.action_search ->{
-                Log.d("TEST", "ICON SEARCH")
-                true
-            }
-        }
-        return super.onOptionsItemSelected(item)
-    }
-
 }
+
+
