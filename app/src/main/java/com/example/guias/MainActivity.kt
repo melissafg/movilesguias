5a5aba90b92873fd60644655265e9d592988102c
melissafg <melissa.flores.g@ucb.edu.bo>
2022-06-01 10:13:52 -0400

    pasos de la guia

--- app/src/main/java/com/example/guias/MainActivity.kt
+++ app/src/main/java/com/example/guias/MainActivity.kt
@@ -1,38 +1,15 @@
 package com.example.guias
 
-import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.Toast
-import androidx.constraintlayout.widget.ConstraintLayout
-
 
 class MainActivity : AppCompatActivity() {
-
-    private val redBtn : Button
-        get() = findViewById(R.id.btnRed)
-
-    private val whiteBtn : Button
-        get() = findViewById(R.id.btnWhite)
-
-    private val layoutPrincipal : ConstraintLayout
-        get() = findViewById(R.id.layoutPrincipal)
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        redBtn.setOnClickListener{
-            toast("Rojo")
-            layoutPrincipal.setBackgroundColor(Color.RED)
-        }
-
-        whiteBtn.setOnClickListener{
-            toast("Blanco")
-            layoutPrincipal.setBackgroundColor(Color.WHITE)
+        supportActionBar.let { it?.setDisplayHomeAsUpEnabled(true)
+            it?.setDisplayShowHomeEnabled(true)
         }
     }
-    private fun toast(text: String, duration: Int = Toast.LENGTH_LONG)=
-        Toast.makeText(this@MainActivity, text, Toast.LENGTH_LONG).show()
 }
